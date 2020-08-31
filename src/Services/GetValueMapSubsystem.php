ef30c8c949079aa0915233a6cb214467fc8b4778
Sergey Zinchenko <s.zinchenko@e-mba.ru>
2020-08-31 15:18:49 +0300

    Do not merge data

--- src/Services/GetValueMapSubsystem.php
+++ src/Services/GetValueMapSubsystem.php
@@ -31,9 +31,8 @@ class GetValueMapSubsystem extends SetValueMapSubsystem implements IGetValueMapS
      */
     public function process(EventData $eventData): EventData
     {
-        $data = $eventData->getData();
         $key = $eventData->get('key');
-        $data = array_merge($data, $this->getValueMapperService()->get($this->getProcessOptions()->getOptions()['namespace'] ?? 'default', $key));
+        $data = $this->getValueMapperService()->get($this->getProcessOptions()->getOptions()['namespace'] ?? 'default', $key);
         $eventData->setData($data);
 
         return $eventData;
